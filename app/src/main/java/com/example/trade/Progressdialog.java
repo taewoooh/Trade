2b8e281618519f652601f5b4bfb851c6a5b09698
taewoooh <taewoooh88@gmail.com>
2023-07-12 08:47:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/trade/Progressdialog.java
@@ -0,0 +1,15 @@
+package com.example.trade;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Window;
+
+import androidx.annotation.NonNull;
+
+public class Progressdialog extends Dialog {
+    public Progressdialog(@NonNull Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_progress);
+    }
+}
